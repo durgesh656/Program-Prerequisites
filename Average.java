578a24e9a508247ba9439441a65289d890bcdb8a
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-08 11:38:36 +0530

    solved 9th question

---
+++ Average.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Average{
+	public static void main(String[]args){
+	
+		Scanner sc=new Scanner(System.in);
+		
+		int n1=sc.nextInt();
+		
+		int n2=sc.nextInt();
+		
+		int n3=sc.nextInt();
+		
+		double avg=(n1+n2+n3)/3;
+		
+		System.out.println("The average of 3 no is: "+avg);
+		}
+	}
