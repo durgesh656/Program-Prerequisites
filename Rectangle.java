b02193670edddba322dd4046862f96e7a4bb5a79
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-08 11:09:48 +0530

    solved 7th question

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Rectangle{
+	public static void main(String[]args){
+	
+		Scanner sc=new Scanner(System.in);
+		
+	
+		int length=sc.nextInt();
+		
+		int width=sc.nextInt();
+		
+		int perimeter=2*(length+width);
+		
+		System.out.println("The perimeter is: "+perimeter);
+	
+		}
+	}
